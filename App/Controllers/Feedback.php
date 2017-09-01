86f01a74735d8cfa825bd5b6e534bb05bc79fef4
Vitaliy <ist70@mail.ru>
2017-09-01 17:25:05 +0400

    Отправка с вложением

--- App/Controllers/Feedback.php
+++ App/Controllers/Feedback.php
@@ -8,90 +8,17 @@ use App\Core\Mail\SendMail;
 class Feedback extends Controller
 {
 
-    public function actionSend2()
+    public function actionSend()
     {
         $name = $_POST['name'];
         $mail = $_POST['mail'];
         $phone = $_POST['phone'];
-        $send = $_POST['send'];
+        $send = 'Телефон: ' . $phone . '<br>' .$_POST['send'];
         $file = $_POST['file'];
+        $filename = basename($_POST['filename']);
         $sendmail = new SendMail();
-        $res = $sendmail->send2($mail, $name, $send);
-        var_dump($res);
+        $res = $sendmail->send($mail, $name, $send, $file, $filename);
         return $res;
-//        $message = Swift_Message::newInstance()
-//            ->setSubject('Your subject')
-//            ->setFrom(array('webmaster@mysite.com' => 'Web Master'))
-//            ->setTo(array('receiver@example.com'))
-//            ->setBody('Here is the message itself');
-//
-//        foreach( $_FILES["ufile"]["tmp_name"] as $tmp_name){
-//            $message->attach(Swift_Attachment::fromPath($tmp_name));
-//        }
-//
-////send the message
-//        $mailer->send($message);
-    }
-
-//    public function actionSend()
-//    {
-//        $name = $_POST['name'];
-//        $mail = $_POST['mail'];
-//        $phone = $_POST['phone'];
-//        $send = $_POST['send'];
-//        $file = $_POST['file'];
-////        $sendmail = new SendMail();
-////        $res = $sendmail->send($mail, $name, $send);
-////        var_dump($res);
-//
-//        ;
-//
-//        die;
-//    }
-
-    /*
-$to - адрес получателя письма
-$from_mail - адрес отправителя письма
-$from_name - имя отправителя письма
-$subject - тема письма
-$message - само сообщение в HTML-формате
-$file_name - путь к файлу, который надо прикрепить к письму
-(это может быть имя файла, выбранного в поле <input type="file" name="file_name">)
-*/
-   public function actionSend()
-    {
-        $from_mail = 'ist70@mail.ru';
-        $from_name = $_POST['name'];
-        $to = $_POST['mail'];
-        $subject = $_POST['phone'];
-        $message = $_POST['send'];
-        $file = $_POST['file'];
-
-        $bound = "spravkaweb-1234";
-        $header = "From: '$from_name' <$from_mail>n";
-        $header .= "To: $to";
-        $header .= "Subject: $subject";
-        $header .= "Mime-Version: 1.0n";
-        $header .= "Content-Type: multipart/mixed; boundary='$bound'";
-        $body = "nn--$bound";
-        $body .= "Content-type: text/html; charset='utf-8'\n";
-        $body .= "Content-Transfer-Encoding: quoted-printablenn";
-        $body .= "$message";
-        //$file = fopen($file_name, "rb");
-        $body .= "nn--$bound";
-        $body .= "Content-Type: application/octet-stream;";
-        $body .= "name=" . basename('file') . "\n";
-        $body .= "Content-Transfer-Encoding:base64n";
-        $body .= "Content-Disposition:attachmentnn";
-        $body .= base64_encode($file) . "n";
-//        $body .= base64_encode(fread($file, filesize($file_name))) . "n";
-        $body .= "$bound--nn";
-       var_dump($body);
-        if (mail($to, $subject, $body, $header)) {
-            echo 'Письмо было успешно отправлено!';
-        } else {
-            echo 'Сообщение не отправлено!';
-        };
     }
 
 }
--- App/Core/Mail/SendMail.php
+++ App/Core/Mail/SendMail.php
@@ -5,48 +5,27 @@ namespace App\Core\Mail;
 class SendMail
 {
 
-    public static function send($to, $title, $mess)
-    {
-        $transport = \Swift_SmtpTransport::newInstance('smtp.mail', 465, 'ssl')
-            ->setUsername('ist70')
-            ->setPassword('');
-
-        $mailer = \Swift_Mailer::newInstance($transport);
-
-        $message = \Swift_Message::newInstance()
-            ->setFrom('ist70@mail.ru')
-            ->setTo($to)
-            ->setSubject($title)
-            ->setBody($mess);
-
-        return $mailer->send($message);
-    }
-
-    public function send2($to, $subject, $message, $from = '', $reply_to = '')
+    public function send($to, $subject, $send, $file, $filename, $from = '', $reply_to = '')
     {
         $transport = \Swift_SmtpTransport::newInstance('smtp.mail.ru', 465, 'ssl')
             ->setUsername('ist70@mail.ru')
-            ->setSourceIp('0.0.0.0')
-            ->setPassword('');
-
-//            ->setUsername('ist70@mail.ru')
-//            ->setPassword('qwertyQQQ1');
-
-
+            ->setPassword('qwertyQQQ1');
 
         $message = \Swift_Message::newInstance($subject)
             ->setFrom('ist70@mail.ru')
             ->setTo($to)
             ->setContentType("text/html; charset=UTF-8")
-            ->setBody($message, 'text/html');
+            ->setBody($send, 'text/html');
+        $message->attach(\Swift_Attachment::newInstance($file, $filename));
 
-//        $result = $mailer->send($messages);
         try{
             $mailer = \Swift_Mailer::newInstance($transport);
-            $response = $mailer->send($message);
+            $res = $mailer->send($message);
         }catch(\Swift_TransportException $e){
             $response = $e->getMessage() ;
             var_dump($response);
         }
+        return $res;
     }
+
 }
